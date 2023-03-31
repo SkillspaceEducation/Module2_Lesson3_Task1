b64cfd80df78df7a4b439a4938d6e68dea2d7f56
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-31 12:57:41 +0300

    Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 /*
 Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания.
 Подсчитать общее их количество.
  */
 public class Main {
     public static void main(String[] args) {
-        String str = "!?,.!";
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
         char[] c = str.toCharArray();
         int num = 0;
         for (char value : c) {
@@ -12,6 +16,6 @@ public class Main {
                 num++;
             }
         }
-        System.out.println(num);
+        System.out.println("Количество знаков препинания в строке:\n" + str + "\nравно " + num);
     }
 }
