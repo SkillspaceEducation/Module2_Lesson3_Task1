7ca299fd8159435a9f0bb97dc7dc881ec911d412
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-31 12:51:27 +0300

    Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+/*
+Введите с клавиатуры строку. Найти в строке не только запятые, но и другие знаки препинания.
+Подсчитать общее их количество.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "!?,.!";
+        char[] c = str.toCharArray();
+        int num = 0;
+        for (char value : c) {
+            if (value == '!' || value == '?' || value == ',' || value == '.') {
+                num++;
+            }
+        }
+        System.out.println(num);
     }
 }
